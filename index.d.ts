ef2b8dcd41d8fbb982ec0bbed1b0db734c08b06f
Andrew Heuermann <aheuermann@gmail.com>
2020-08-04 11:41:56 -0500

    Allow arrays of arrays in `intoStream.object` TypeScript type definition (#15)

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,8 @@ declare namespace intoStream {
 		| Iterable<Buffer | string>;
 
 	type InputObject =
-		| {[key: string]: any}
-		| Iterable<{[key: string]: any}>;
+		| object
+		| Iterable<object>;
 }
 
 declare const intoStream: {
