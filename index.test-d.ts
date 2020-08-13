59bc3b7cd86a68e8aed8c45bcf262bb4aae701ed
papb <papb1996@gmail.com>
2020-08-13 00:25:33 -0300

    Support async iterables

--- index.test-d.ts
+++ index.test-d.ts
@@ -2,6 +2,18 @@ import intoStream = require('.');
 
 const unicornArray = 'unicorn'.split('');
 
+function asyncGeneratorFrom<T>(array: T[]) {
+	return async function * () {
+		yield array[0];
+	};
+}
+
+function asyncIterableFrom<T>(array: T[]) {
+	return {
+		[Symbol.asyncIterator]: asyncGeneratorFrom(array)
+	};
+}
+
 intoStream('unicorn').pipe(process.stdout);
 intoStream(unicornArray).pipe(process.stdout);
 intoStream(new Set(unicornArray)).pipe(process.stdout);
@@ -22,8 +34,12 @@ intoStream(Promise.resolve(new Uint8Array(Buffer.from('unicorn').buffer))).pipe(
 	process.stdout
 );
 
+intoStream(asyncGeneratorFrom(unicornArray)()).pipe(process.stdout);
+intoStream(asyncIterableFrom(unicornArray)).pipe(process.stdout);
+intoStream(Promise.resolve(asyncIterableFrom(unicornArray))).pipe(process.stdout);
+
 const object = {foo: true};
-const objectArray = new Set([object, {bar: true}]);
+const objectArray = [object, {bar: true}];
 const objectIterable = new Set(objectArray);
 const arrayOfArrays = [[object]];
 
@@ -35,3 +51,6 @@ intoStream.object(Promise.resolve(object)).pipe(process.stdout);
 intoStream.object(Promise.resolve(objectArray)).pipe(process.stdout);
 intoStream.object(Promise.resolve(objectIterable)).pipe(process.stdout);
 intoStream.object(Promise.resolve(arrayOfArrays)).pipe(process.stdout);
+intoStream.object(asyncGeneratorFrom(objectArray)()).pipe(process.stdout);
+intoStream.object(asyncIterableFrom(objectArray)).pipe(process.stdout);
+intoStream.object(Promise.resolve(asyncIterableFrom(objectArray))).pipe(process.stdout);
--- test.js
+++ test.js
@@ -1,18 +1,40 @@
 import test from 'ava';
 import getStream from 'get-stream';
 import pEvent from 'p-event';
+import pImmediate from 'p-immediate';
 import intoStream from '.';
 
 const fixture = 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.';
 
+function generatorFrom(array) {
+	return function * () {
+		let i = 0;
+		while (i < array.length) {
+			yield array[i++];
+		}
+	};
+}
+
 function iterableFrom(array) {
 	return {
-		* [Symbol.iterator]() {
-			let i = 0;
-			while (i < array.length) {
-				yield array[i++];
-			}
+		[Symbol.iterator]: generatorFrom(array)
+	};
+}
+
+function asyncGeneratorFrom(array) {
+	return async function * () {
+		let i = 0;
+		while (i < array.length) {
+			// eslint-disable-next-line no-await-in-loop
+			await pImmediate();
+			yield array[i++];
 		}
+	};
+}
+
+function asyncIterableFrom(array) {
+	return {
+		[Symbol.asyncIterator]: asyncGeneratorFrom(array)
 	};
 }
 
@@ -54,6 +76,21 @@ test('iterable', async t => {
 	t.is(await getStream(intoStream(iterable)), fixture);
 });
 
+test('generator', async t => {
+	const generator = generatorFrom(fixture.split(''));
+	t.is(await getStream(intoStream(generator())), fixture);
+});
+
+test('async iterable', async t => {
+	const iterable = asyncIterableFrom(fixture.split(''));
+	t.is(await getStream(intoStream(iterable)), fixture);
+});
+
+test('async generator', async t => {
+	const generator = asyncGeneratorFrom(fixture.split(''));
+	t.is(await getStream(intoStream(generator())), fixture);
+});
+
 test('promise', async t => {
 	const promise = new Promise(resolve => {
 		setImmediate(resolve.bind(null, fixture));
@@ -64,6 +101,13 @@ test('promise', async t => {
 test('promise resolving to iterable', async t => {
 	const promise = new Promise(resolve => {
 		setImmediate(resolve.bind(null, iterableFrom(fixture.split(''))));
+	});
+	t.is(await getStream(intoStream(promise)), fixture);
+});
+
+test('promise resolving to async iterable', async t => {
+	const promise = new Promise(resolve => {
+		setImmediate(resolve.bind(null, asyncIterableFrom(fixture.split(''))));
 	});
 	t.is(await getStream(intoStream(promise)), fixture);
 });
@@ -89,6 +133,12 @@ test('object mode from iterable', async t => {
 	t.deepEqual(await getStream.array(intoStream.object(iterable)), values);
 });
 
+test('object mode from async iterable', async t => {
+	const values = [{foo: true}, {bar: true}];
+	const iterable = asyncIterableFrom(values);
+	t.deepEqual(await getStream.array(intoStream.object(iterable)), values);
+});
+
 test('object mode from promise', async t => {
 	const f = {foo: true};
 	const promise = new Promise(resolve => {
@@ -107,6 +157,14 @@ test('object mode from promise resolving to iterable', async t => {
 	const values = [{foo: true}, {bar: true}];
 	const promise = new Promise(resolve => {
 		setImmediate(resolve.bind(null, iterableFrom([{foo: true}, {bar: true}])));
+	});
+	t.deepEqual(await getStream.array(intoStream.object(promise)), values);
+});
+
+test('object mode from promise resolving to async iterable', async t => {
+	const values = [{foo: true}, {bar: true}];
+	const promise = new Promise(resolve => {
+		setImmediate(resolve.bind(null, asyncIterableFrom([{foo: true}, {bar: true}])));
 	});
 	t.deepEqual(await getStream.array(intoStream.object(promise)), values);
 });
